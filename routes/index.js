251000b5c1125f87c42df2465e3145057d99edff
Cecilio Pared <113487239+cecilioPared@users.noreply.github.com>
2022-10-11 01:30:06 -0300

    Delete routes directory

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express')
-const producto = require('./producto')
-const mensaje = require('./mensaje')
-const router = Router()
-
-router.use(producto, mensaje)
-
-module.exports = router
